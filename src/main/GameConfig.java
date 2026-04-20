53a26b21761911cf1b7c944c12e0ff36c4e8a87e
LOQ <LOQ@LAPTOP-4R9N2ICI>
2026-04-21 01:00:06 +0700

    fix map

--- src/main/GameConfig.java
+++ src/main/GameConfig.java
@@ -5,7 +5,7 @@ import entity.TankType;
 public class GameConfig {
     public int playerCount = 1;
     public int computerCount = 0;
-    public String mapPath = "/maps/map03.txt";
+    public String mapPath = "/maps/map01.txt";
     public String gameMode = "Deathmatch";
     public boolean startImmediately = false;
     
